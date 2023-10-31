13ba3a4d681e6e215b56641dcba4e3f8b239cfcd
A859263 <roman.lotnik.ext@auxo.team>
2023-10-31 15:50:23 +0300

    init

---
+++ config/src/test/java/com/auxomarket/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auxomarket.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
